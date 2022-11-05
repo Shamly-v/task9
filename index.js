179323e54e0f5f44b75e036c634682c4e496b9f6
shamly-v <shamlyhameed2003@gmail.com>
2022-11-05 10:07:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const navbar = (
+    <nav>
+        <h1>Social Networking</h1>
+        <ul>
+            <li>Google</li>
+            <li>Twitter</li>
+            <li>Linkedin</li>
+            <li>Facebook</li>
+        </ul>
+
+    </nav>
+)
+ReactDOM.render(navbar,document.getElementById("root"))
